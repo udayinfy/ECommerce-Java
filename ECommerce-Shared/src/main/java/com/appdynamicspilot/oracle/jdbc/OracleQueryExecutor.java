e616c59b6edc097382f114c724d7d09418d78d5c
aleftik <aleftik@appdynamics.com>
2015-03-08 11:33:01 -0700

    Fixes for Query Return

--- ECommerce-Shared/src/main/java/com/appdynamicspilot/oracle/jdbc/OracleQueryExecutor.java
+++ ECommerce-Shared/src/main/java/com/appdynamicspilot/oracle/jdbc/OracleQueryExecutor.java
@@ -1,46 +1,23 @@
-package com.appdynamicspilot.oracleJDBC;
+package com.appdynamicspilot.oracle.jdbc;
 
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.SQLException;
+
 
 import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.CallableStatementCallback;
-import org.springframework.jdbc.core.CallableStatementCreator;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+
 
 /**
  * 
  * @author Sharat Jagannath
  *
  */
-public class abstract OracleQueryExecutor {
+public abstract class OracleQueryExecutor {
     private final static Logger LOGGER = Logger.getLogger(OracleQueryExecutor.class.getName());
     private String oracleQueryString;
     private DataSource ds  = null;
 
-    
-//    public void executeOracleQuery() {
-//			Connection connection = null;
-//			CallableStatement cs = null;
-//			try {
-//				connection = ds.getConnection();
-//				cs = connection.prepareCall("{ call getItem(?) }");
-//                cs.setInt(1,200);
-//				cs.execute();
-//			} catch (Exception ex) {
-//				LOGGER.error("This may be ignored in case of Oracle is not setup");
-//				LOGGER.error(ex.getMessage());
-//			} finally {
-//				if (cs != null) try {cs.close();} catch (Exception ex) {}
-//				if (connection != null) try {connection.close();} catch (Exception ex) {}
-//			}
-//		LOGGER.info("Done");
-//    }
+
 
     public abstract void  executeOracleQuery();
     
@@ -55,5 +32,8 @@ public class abstract OracleQueryExecutor {
     public void setDataSource(DataSource dataSource) {
        this.ds = dataSource;
     }
+    public DataSource getDataSource() {
+        return this.ds;
+    }
 
-}
+};
