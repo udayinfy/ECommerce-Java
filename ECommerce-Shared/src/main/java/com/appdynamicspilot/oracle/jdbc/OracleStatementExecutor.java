5ed308c529877086d03aa94df5891231a1fc58f7
aleftik <aleftik@appdynamics.com>
2015-03-08 12:01:08 -0700

    Query fixes

--- ECommerce-Shared/src/main/java/com/appdynamicspilot/oracle/jdbc/OracleStatementExecutor.java
+++ ECommerce-Shared/src/main/java/com/appdynamicspilot/oracle/jdbc/OracleStatementExecutor.java
@@ -1,7 +1,34 @@
 package com.appdynamicspilot.oracle.jdbc;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.apache.log4j.Logger;
+
 /**
  * Created by aleftik on 3/8/15.
  */
-public class OracleStatementExecutor {
+public class OracleStatementExecutor extends OracleQueryExecutor {
+    private static final Logger logger = Logger.getLogger(OracleStatementExecutor.class.getName());
+
+
+
+    @Override
+    public void executeOracleQuery() {
+        Connection connection = null;
+        Statement stmt = null;
+        try {
+            connection = getDataSource().getConnection();
+            stmt = connection.createStatement();
+            stmt.execute(getOracleQueryString());
+        } catch (SQLException sqle) {
+            logger.error("This may be ignored in case of Oracle is not setup");
+            logger.error(sqle.getMessage());
+        }  finally {
+            if (connection != null) {try{connection.close();}catch (SQLException sqle) {}}
+            if (stmt != null) {try{stmt.close();}catch (SQLException sqle) {}}
+        }
+
+
+    }
 }
